e3a2ba3b190b5b01ddba368b7b99c516fc83ebcb
Ruzell11 <johnruzell123@gmail.com>
2023-09-04 10:55:37 +0800

    imlpement create user

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './modules/login';
 import SignUp from './modules/sign-up';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
 
 function App() {
+  const queryClient = new QueryClient()
   return (
-    <Router>
+   <QueryClientProvider client={queryClient}>
+     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
     </Router>
+   </QueryClientProvider>
   );
 }
 
