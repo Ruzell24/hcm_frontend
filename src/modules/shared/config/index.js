9ed8ff3909f623beb03fb2d6d22babc723eb9cfe
Ruzell11 <johnruzell123@gmail.com>
2023-09-07 13:12:12 +0800

    fix env variable not reading

--- src/modules/shared/config/index.js
+++ src/modules/shared/config/index.js
@@ -1,3 +1,3 @@
 export default {
-    API_URL_BASE: "http://localhost:3000",
+    API_URL_BASE: import.meta.env.VITE_SERVER === "development" ? "http://localhost:3000" : "http://137.184.22.47:3000"
 };
